6e591a54a84889d491996a9181a2324c0a365f71
Kevin-Olascoaga <kevinolascoaga@outlook.com>
2019-08-20 16:56:49 -0500

    geocoder

--- LambdaSNS/geocoder.js
+++ LambdaSNS/geocoder.js
@@ -1,4 +1,27 @@
+var Request = require("request");
+
 exports.handler = function(event, context, callback) {
     
-    callback(null, {"message": "Successfully executed"});
+    let lat = event['lat'];
+    let lon = event['lon'];
+
+    var data = geocoder(lat,lon);
+    
+    callback(null, data);
+}
+
+function geocoder(lat,lon){
+    Request.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyB_H0OkKqjnld_RXVKXClJbENg4xO4eVxQ", (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        let api = JSON.parse(body);
+        let direccion = api.results[0];
+
+        console.log(direccion);
+
+        return direccion;
+
+
+    });
 }
