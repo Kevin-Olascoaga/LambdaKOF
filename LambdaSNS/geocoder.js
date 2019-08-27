52d86b8152e8bbec68929d3e6bbfa71f72532780
Kevin-Olascoaga <kevinolascoaga@outlook.com>
2019-08-27 10:58:12 -0500

    geocoder

--- LambdaSNS/geocoder.js
+++ LambdaSNS/geocoder.js
@@ -4,24 +4,50 @@ exports.handler = function(event, context, callback) {
     
     let lat = event['lat'];
     let lon = event['lon'];
-
-    var data = geocoder(lat,lon);
+    let geocoder = {
+        numero: "",
+        calle: "",
+        colonia: "",
+        municipio: "",
+        estado: "",
+        pais: "",
+        codigoPostal: ""
+    }
     
-    callback(null, data);
-}
-
-function geocoder(lat,lon){
     Request.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyB_H0OkKqjnld_RXVKXClJbENg4xO4eVxQ", (error, response, body) => {
         if(error) {
             return console.dir(error);
         }
         let api = JSON.parse(body);
         let direccion = api.results[0];
-
+        let dir = direccion.address_components
+        
+        dir.forEach(function(element) {
+            if (element.types[0] == "street_number") {
+                geocoder.numero = element.long_name;
+            }
+            if (element.types[0] == "route") {
+                geocoder.calle = element.long_name;
+            }
+            if (element.types[0] == "neighborhood") {
+                geocoder.municipio = element.long_name;
+            }
+            if (element.types[0] == "political") {
+                geocoder.colonia = element.long_name;
+            }
+            if (element.types[0] == "locality") {
+                geocoder.estado = element.long_name;
+            }
+            if (element.types[0] == "country") {
+                geocoder.pais = element.long_name;
+            }
+            if (element.types[0] == "postal_code") {
+                geocoder.codigoPostal = element.long_name;
+            }
+            console.log(element)
+            console.log(geocoder)
+        })
         console.log(direccion);
-
-        return direccion;
-
-
+        callback(null, geocoder);
     });
 }
